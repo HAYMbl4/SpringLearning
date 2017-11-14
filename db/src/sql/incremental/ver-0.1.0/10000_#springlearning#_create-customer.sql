6232b6a17f60deb5a37b4d0f1fe66a4abdf966a6
Oleg Naumov <o.naumov@argustelecom.ru>
2017-11-14 11:35:55 +0300

    Добавил секвенцию для идентификаторов

--- db/src/sql/incremental/ver-0.1.0/10000_#springlearning#_create-customer.sql
+++ db/src/sql/incremental/ver-0.1.0/10000_#springlearning#_create-customer.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS springlearning.customer (
-  id          BIGINT       NOT NULL,
+  id          BIGINT       NOT NULL DEFAULT nextval('springlearning.gen_object_id'),
   first_name  VARCHAR(256) NOT NULL,
   second_name VARCHAR(256),
   last_name   VARCHAR(256),
